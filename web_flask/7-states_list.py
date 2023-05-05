68bdc155f887be69a54c52a3df56abd4dd15219c
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-05 11:41:28 +0100

    display list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Flask web application to display a list of states.
+"""
+from flask import Flask 
+from flask import render_template
+from models import storage, State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with the list of all State objects."""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
