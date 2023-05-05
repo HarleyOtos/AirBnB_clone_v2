9bd5e1678c581c86568e9f5d476c6b66d5add31e
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-05 11:59:36 +0100

    Display states and cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def display_states_and_cities():
+    """Displays all the states and cities"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/9-states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage, State, City
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Closes the database session after each request"""
+    storage.close()
+
+
+@app.route('/states')
+def display_states():
+    """Displays all states and their cities"""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>')
+def display_state(id):
+    """Displays a state and its cities"""
+    state = storage.get(State, id)
+    if state is None:
+        return render_template('9-not_found.html')
+    else:
+        cities = sorted(state.cities, key=lambda x: x.name)
+        return render_template('9-states.html', state=state, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
